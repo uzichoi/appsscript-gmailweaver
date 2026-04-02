47539cb4d3da3bc0d6bb08c559905f8458d08612
uzichoi <cjiyou1090@gmail.com>
2026-04-02 12:08:39 +0900

    docs: gitignore 공백 제거

--- src/apps-script/common.js
+++ src/apps-script/common.js
@@ -1,7 +1,7 @@
 // src/apps-script/common.js
-var TunnelURL = "https://unmatching-sandy-hydrocinnamyl.ngrok-free.dev"; // ngrok로 열어둔 백엔드 서버(Flask/GraphRAG) 주소
+var TunnelURL = "https://transpalmar-christine-noneducatory.ngrok-free.dev"; // ngrok로 열어둔 백엔드 서버(Flask/GraphRAG) 주소
 const WEBAPP_URL =
-  "https://script.google.com/macros/s/AKfycbzR29ycMGq8ig5H8NMB4fciIwTleDtN-7UJKH-agPx_uK3tN4yKtkfe9v0lZ_kAvS8a/exec"; // Apps Script Web App으로 배포된 URL
+  "https://script.google.com/macros/s/AKfycbz3bAOxML5BZSSJcMFM1or5jY8K4NVwliHk_Rbe9jXYVBXbYM05Fl-1bPG1909_38hZ/exec"; // Apps Script Web App으로 배포된 URL
 
 const OLIVE = "#c6d8a5";
 
